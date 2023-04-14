be0800f676dd34933fb108d5028e43b060eaacf9
amauryval <amauryval@gmail.com>
2023-04-14 22:43:02 +0200

    upd (routes) path add descriptions

--- network_api/routers/path/routes.py
+++ network_api/routers/path/routes.py
@@ -7,8 +7,13 @@ from starlite import Controller, get, Router
 
 class ShortestPathController(Controller):
 
-    @get("/shortest", dependencies={})
+    @get("/shortest",
+         operation_id="shortest path",
+         summary="Compute a shortest path from wkt points (2 at least)",
+         response_description="Returns a list of coordinates based on the shortest path found"
+         )
     async def get_shortest_path(self, mode: str, step_point: List[str]) -> List[Tuple[float, float]]:
+        """Compute a shortest path from wkt point"""
         roads_object = GraphAnalysis(mode, list(map(lambda coords: loads(coords), step_point)))
         paths_found = roads_object.get_shortest_path()
         return list(paths_found)[0].path.coords[:]
@@ -16,6 +21,7 @@ class ShortestPathController(Controller):
 
 router = Router(
     path="/path",
+    tags=["path"],
     route_handlers=[ShortestPathController],
     dependencies={}
 )
